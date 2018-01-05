276087627120db7788481e89d7c87b8d1ad3640c
Matthew Cox-Martin <vtmacox@gmail.com>
2018-01-05 16:33:26 -0700

    Changed files to get help file to display properly

---
+++ R/chordsTables.R
@@ -0,0 +1,8 @@
+#' chordsTables: A package for generating priority tables for the CHORDS group.
+#'
+#' The chordsTables package uses the run_report function to generate the specified
+#' priority table report.
+#'
+#' @docType package
+#' @name chordsTables
+NULL
